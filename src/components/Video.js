75d6b7877873fb51350d69402b59016315053ded
HaudegenHH <einsundeins79@gmx.de>
2022-09-18 00:27:58 +0200

    welcome page & navbar

---
+++ src/components/Video.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './VideoStyles.css'
+import { Link } from 'react-router-dom'
+
+import SpaceVideo from '../assets/space.mp4'
+
+const Video = () => {
+  return (
+    <div className="hero">
+      <video autoPlay muted loop id="video">
+        <source src={SpaceVideo} type="video/mp4" />
+      </video>
+      <div className="content">
+        <h1>Galaxy. Travel.</h1>
+        <p>World's first civilian space travel</p>      
+        <div>
+          <Link to="/skills" className="btn">Skills</Link>
+          <Link to="/projects" className="btn btn-light">Projects</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Video
